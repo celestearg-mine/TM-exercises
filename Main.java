4cfafd6a6f35278cf025992df6069bfb1432c722
Celeste Tello <celeste-tello@CelesteTello.com>
2022-03-02 11:38:59 -0600

    Start

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author Celeste Tello.
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        Circle c = new Circle("Black", 2.5);
+        Rectangle r = new Rectangle("Pink", 13.2, 8);
+        Triangle t = new Triangle("Yellow", 15,3);
+
+        System.out.println("\n CIRCLE VALUES \n"+c.toString()+"\n");
+        System.out.println(" RECTANGLE VALUES \n"+r.toString()+"\n");
+        System.out.println(" TRIANGLE VALUES \n"+t.toString()+"\n");
+    }
+}
---
+++ Shape.java
@@ -0,0 +1,24 @@
+import java.text.DecimalFormat;
+
+/**
+ * @author Celeste Tello.
+ */
+
+abstract class Shape{
+
+    static String color;
+
+    DecimalFormat df = new DecimalFormat("###,###.00");
+
+    public abstract double area();
+
+    public abstract String toString();
+
+    public String getColor(){
+        return color;
+    }
+
+
+
+
+}
